7d8c3c3e52c709763fd36e538a468fc2a2ba037e
[Jie Dai] <[djay1128@yahoo.com]>
2024-04-01 13:27:22 -0400

    Add UI components and implement basic functionality in MainActivity

--- app/src/main/java/com/laioffer/spotify/MainActivity.kt
+++ app/src/main/java/com/laioffer/spotify/MainActivity.kt
@@ -1,22 +1,43 @@
 package com.laioffer.spotify
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.laioffer.spotify.ui.theme.SpotifyTheme
 
 // customized extend AppCompatActivity
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "lifecycle"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // declare UI component
+        // fetch data
+
         setContent {
             SpotifyTheme {
                 // A surface container using the 'background' color from the theme
@@ -24,18 +45,110 @@ class MainActivity : AppCompatActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    var name by remember { mutableStateOf("") }
+                    HelloContent(name) { name = it }
                 }
             }
         }
+
+        // tag, message
+        Log.d(TAG, "We are at onCreate()")
+    }
+
+}
+
+
+
+// get
+@Composable
+private fun HelloContent(name: String, onNameChange: (String) -> Unit) {
+    Column(modifier = Modifier.padding(16.dp)) {
+        if (name.isNotEmpty()) {
+            Text(
+                text = "Hello!",
+                modifier = Modifier.padding(bottom = 8.dp),
+                style = MaterialTheme.typography.body2
+            )
+        }
+
+        OutlinedTextField(
+            value = name, // state
+            onValueChange = onNameChange,
+            label = { Text("Name") }
+        )
+    }
+}
+
+
+@Composable
+fun ArtistCardBox() {
+    Box {
+        Text("Alfred Sisley")
+        Text("3 minutes ago")
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ArtistCardColumn() {
+    Column {
+        Text("Alfred Sisley")
+        Text("3 minutes ago")
+        Text("laioffer")
+    }
 }
 
+@Composable
+fun ArtistCardRow() {
+    Row {
+        Text("Alfred Sisley")
+        Text("3 minutes ago")
+    }
+}
+
+@Preview
+@Composable
+fun PreviewArtistCardColumn() {
+    SpotifyTheme {
+        Surface {
+            ArtistCardColumn()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewArtistCardBox() {
+    SpotifyTheme {
+        Surface {
+            ArtistCardBox()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewArtistCardRow() {
+    SpotifyTheme {
+        Surface {
+            ArtistCardRow()
+        }
+    }
+}
+
+
+@Composable
+private fun Greeting(name: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(24.dp)
+            .background(Color.Red)
+    ) {
+        Text(text = "Hello,")
+        Text(text = name)
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
@@ -45,3 +158,4 @@ fun DefaultPreview() {
         }
     }
 }
+
