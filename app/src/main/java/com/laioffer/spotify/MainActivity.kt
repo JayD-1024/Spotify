ab1cf21a263274a675631d27a7e652c7df3db0ac
[Jie Dai] <[djay1128@yahoo.com]>
2024-04-04 12:18:44 -0400

    Design Album Cover

--- app/src/main/java/com/laioffer/spotify/MainActivity.kt
+++ app/src/main/java/com/laioffer/spotify/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
@@ -21,10 +23,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.laioffer.spotify.ui.theme.SpotifyTheme
 
 // customized extend AppCompatActivity
@@ -45,8 +51,7 @@ class MainActivity : AppCompatActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    var name by remember { mutableStateOf("") }
-                    HelloContent(name) { name = it }
+                    AlbumCover()
                 }
             }
         }
@@ -56,7 +61,6 @@ class MainActivity : AppCompatActivity() {
     }
 
 }
-
 
 
 // get
@@ -72,9 +76,37 @@ private fun HelloContent(name: String, onNameChange: (String) -> Unit) {
         }
 
         OutlinedTextField(
-            value = name, // state
+            value = name, // getState
             onValueChange = onNameChange,
             label = { Text("Name") }
+        )
+    }
+}
+
+@Composable
+private fun AlbumCover() {
+    Column {
+        Box(modifier = Modifier.size(160.dp)) {
+            // "https://upload.wikimedia.org/wikipedia/en/d/d1/Stillfantasy.jpg"
+            AsyncImage(
+                model = "https://upload.wikimedia.org/wikipedia/en/d/d1/Stillfantasy.jpg",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.FillBounds,
+                contentDescription = null
+            )
+            Text(
+                text = "Still Fantasy",
+                color = Color.White,
+                modifier = Modifier
+                    .padding(start = 2.dp, bottom = 4.dp)
+                    .align(Alignment.BottomStart)
+            )
+        }
+        Text(
+            text = "Jay Chou",
+            modifier = Modifier.padding(top = 4.dp),
+            style = MaterialTheme.typography.body2.copy(fontWeight = FontWeight.Bold),
+            color = Color.LightGray
         )
     }
 }
@@ -149,6 +181,7 @@ private fun Greeting(name: String) {
         Text(text = name)
     }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
@@ -158,4 +191,3 @@ fun DefaultPreview() {
         }
     }
 }
-
