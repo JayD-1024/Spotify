b5011e9e36f174ba99667cdd685a2c6a2189a9c1
[Jie Dai] <[djay1128@yahoo.com]>
2024-04-04 19:42:05 -0400

    build home fragment, favorite fragment and nav_graph

--- app/src/main/java/com/laioffer/spotify/MainActivity.kt
+++ app/src/main/java/com/laioffer/spotify/MainActivity.kt
@@ -1,193 +1,35 @@
 package com.laioffer.spotify
 
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import com.laioffer.spotify.ui.theme.SpotifyTheme
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-// customized extend AppCompatActivity
 class MainActivity : AppCompatActivity() {
-
-    private val TAG = "lifecycle"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        // declare UI component
-        // fetch data
+        val navView = findViewById<BottomNavigationView>(R.id.nav_view)
 
-        setContent {
-            SpotifyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    AlbumCover()
-                }
-            }
-        }
+        // host
+        val navHostFragment =supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
-        // tag, message
-        Log.d(TAG, "We are at onCreate()")
-    }
+        // controller
+        val navController = navHostFragment.navController
 
-}
+        // graph
+        navController.setGraph(R.navigation.nav_graph)
 
+        NavigationUI.setupWithNavController(navView, navController)
 
-// get
-@Composable
-private fun HelloContent(name: String, onNameChange: (String) -> Unit) {
-    Column(modifier = Modifier.padding(16.dp)) {
-        if (name.isNotEmpty()) {
-            Text(
-                text = "Hello!",
-                modifier = Modifier.padding(bottom = 8.dp),
-                style = MaterialTheme.typography.body2
-            )
-        }
-
-        OutlinedTextField(
-            value = name, // getState
-            onValueChange = onNameChange,
-            label = { Text("Name") }
-        )
-    }
-}
-
-@Composable
-private fun AlbumCover() {
-    Column {
-        Box(modifier = Modifier.size(160.dp)) {
-            // "https://upload.wikimedia.org/wikipedia/en/d/d1/Stillfantasy.jpg"
-            AsyncImage(
-                model = "https://upload.wikimedia.org/wikipedia/en/d/d1/Stillfantasy.jpg",
-                modifier = Modifier.fillMaxSize(),
-                contentScale = ContentScale.FillBounds,
-                contentDescription = null
-            )
-            Text(
-                text = "Still Fantasy",
-                color = Color.White,
-                modifier = Modifier
-                    .padding(start = 2.dp, bottom = 4.dp)
-                    .align(Alignment.BottomStart)
-            )
-        }
-        Text(
-            text = "Jay Chou",
-            modifier = Modifier.padding(top = 4.dp),
-            style = MaterialTheme.typography.body2.copy(fontWeight = FontWeight.Bold),
-            color = Color.LightGray
-        )
-    }
-}
-
-
-@Composable
-fun ArtistCardBox() {
-    Box {
-        Text("Alfred Sisley")
-        Text("3 minutes ago")
-    }
-}
-
-@Composable
-fun ArtistCardColumn() {
-    Column {
-        Text("Alfred Sisley")
-        Text("3 minutes ago")
-        Text("laioffer")
-    }
-}
-
-@Composable
-fun ArtistCardRow() {
-    Row {
-        Text("Alfred Sisley")
-        Text("3 minutes ago")
-    }
-}
-
-@Preview
-@Composable
-fun PreviewArtistCardColumn() {
-    SpotifyTheme {
-        Surface {
-            ArtistCardColumn()
-        }
-    }
-}
-
-@Preview
-@Composable
-fun PreviewArtistCardBox() {
-    SpotifyTheme {
-        Surface {
-            ArtistCardBox()
-        }
-    }
-}
-
-@Preview
-@Composable
-fun PreviewArtistCardRow() {
-    SpotifyTheme {
-        Surface {
-            ArtistCardRow()
-        }
-    }
-}
-
-
-@Composable
-private fun Greeting(name: String) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-            .padding(24.dp)
-            .background(Color.Red)
-    ) {
-        Text(text = "Hello,")
-        Text(text = name)
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SpotifyTheme {
-        Surface {
-            Greeting("Android")
-        }
+//        // https://stackoverflow.com/questions/70703505/navigationui-not-working-correctly-with-bottom-navigation-view-implementation
+//        navView.setOnItemSelectedListener{
+//            NavigationUI.onNavDestinationSelected(it, navController)
+//            navController.popBackStack(it.itemId, inclusive = false)
+//            true
+//        }
     }
 }
