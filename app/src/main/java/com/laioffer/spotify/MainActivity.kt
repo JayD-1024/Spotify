31ad20360d66a62c9e27441cbb17321a7333305e
[Jie Dai] <[djay1128@yahoo.com]>
2024-04-05 23:31:31 -0400

    add and configue network

--- app/src/main/java/com/laioffer/spotify/MainActivity.kt
+++ app/src/main/java/com/laioffer/spotify/MainActivity.kt
@@ -1,35 +1,51 @@
 package com.laioffer.spotify
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.laioffer.spotify.network.NetworkApi
+import com.laioffer.spotify.network.NetworkModule
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "lifecycle"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "We are at onCreate()")
         setContentView(R.layout.activity_main)
 
+        // findElementByClass/Id
         val navView = findViewById<BottomNavigationView>(R.id.nav_view)
 
-        // host
-        val navHostFragment =supportFragmentManager
+        val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
-        // controller
         val navController = navHostFragment.navController
-
-        // graph
         navController.setGraph(R.navigation.nav_graph)
 
         NavigationUI.setupWithNavController(navView, navController)
 
-//        // https://stackoverflow.com/questions/70703505/navigationui-not-working-correctly-with-bottom-navigation-view-implementation
-//        navView.setOnItemSelectedListener{
-//            NavigationUI.onNavDestinationSelected(it, navController)
-//            navController.popBackStack(it.itemId, inclusive = false)
-//            true
-//        }
+        // https://stackoverflow.com/questions/70703505/navigationui-not-working-correctly-with-bottom-navigation-view-implementation
+        navView.setOnItemSelectedListener{
+            NavigationUI.onNavDestinationSelected(it, navController)
+            navController.popBackStack(it.itemId, inclusive = false)
+            true
+        }
+
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val retrofit = NetworkModule.provideRetrofit()
+            val api = retrofit.create(NetworkApi::class.java)
+
+            val response = api.getHomeFeed().execute().body()
+            Log.d(TAG, response.toString())
+        }
     }
 }
