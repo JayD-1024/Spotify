df807245d72beeee7aeecf11adc94d6d38dac8c5
[Jie Dai] <[djay1128@yahoo.com]>
2024-04-20 11:13:22 -0400

    build playlist, modify playerBar, add exoplayer

--- app/src/main/java/com/laioffer/spotify/ui/playlist/PlaylistScreen.kt
+++ app/src/main/java/com/laioffer/spotify/ui/playlist/PlaylistScreen.kt
@@ -1,9 +1,25 @@
 package com.laioffer.spotify.ui.playlist
 
+
 import android.util.Log
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -14,10 +30,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -27,49 +47,73 @@ import coil.compose.AsyncImage
 import com.laioffer.spotify.R
 import com.laioffer.spotify.datamodel.Album
 import com.laioffer.spotify.datamodel.Song
+import com.laioffer.spotify.player.PlayerUiState
+import com.laioffer.spotify.player.PlayerViewModel
+
 
 @Composable
-fun PlaylistScreen(playlistViewModel: PlaylistViewModel) {
+fun PlaylistScreen(
+    playlistViewModel: PlaylistViewModel,
+    playerViewModel: PlayerViewModel
+) {
     val playlistUiState by playlistViewModel.uiState.collectAsState()
-
-    PlaylistScreenContent(
-        playlistUiState = playlistUiState,
+    val playerUiState by playerViewModel.uiState.collectAsState()
+    PlaylistScreenContent(playlistUiState = playlistUiState,
+        playerUiState = playerUiState,
         onTapFavorite = {
-            Log.d("PlaylistScreen", "Tap favorite $it")
             playlistViewModel.toggleFavorite(it)
-        }
-    )
-
+            Log.d("PlaylistScreen", "Tap favorite $it")
+        },
+        onTapSong = {
+            playerViewModel.load(it, playlistUiState.album)
+            playerViewModel.play()
+            Log.d("PlaylistScreen", "Tap song ${it.name}")
+        })
 }
+
 
 @Composable
 private fun PlaylistScreenContent(
     playlistUiState: PlaylistUiState,
-    onTapFavorite: (Boolean) -> Unit
+    playerUiState: PlayerUiState,
+    onTapFavorite: (Boolean) -> Unit,
+    onTapSong: (Song) -> Unit
 ) {
     Column(
-        modifier = Modifier
-            .padding(16.dp),
+        modifier = Modifier.padding(16.dp)
     ) {
+
         Cover(
             album = playlistUiState.album,
             isFavorite = playlistUiState.isFavorite,
-            onTapFavorite = onTapFavorite
+            onTapFavorite = onTapFavorite,
         )
+
         PlaylistHeader(album = playlistUiState.album)
 
-        PlaylistContent(playlist = playlistUiState.playlist)
+        PlaylistContent(
+            playlist = playlistUiState.playlist,
+            currentSong = playerUiState.song,
+            onTapSong = onTapSong
+        )
     }
 }
 
+
 @Composable
 private fun PlaylistContent(
-    playlist: List<Song>
+    playlist: List<Song>,
+    currentSong: Song? = null,
+    onTapSong: (Song) -> Unit
 ) {
     val state = rememberLazyListState()
     LazyColumn(state = state) {
         items(playlist) { song ->
-            Song(song, false )
+            Song(
+                song = song,
+                isPlaying = song == currentSong,
+                onTapSong = onTapSong
+            )
         }
 
         item {
@@ -79,11 +123,15 @@ private fun PlaylistContent(
 
 }
 
+
 @Composable
-private fun Song(song: Song, isPlaying: Boolean) {
+private fun Song(song: Song, isPlaying: Boolean, onTapSong: (Song) -> Unit) {
     Row(
         modifier = Modifier
-            .padding(vertical = 8.dp),
+            .padding(vertical = 8.dp)
+            .clickable {
+                onTapSong(song)
+            },
         verticalAlignment = Alignment.CenterVertically
     ) {
         Column(modifier = Modifier.weight(1.0f)) {
@@ -137,17 +185,32 @@ private fun Cover(
     isFavorite: Boolean,
     onTapFavorite: (Boolean) -> Unit
 ) {
-    Column(
-        modifier = Modifier.fillMaxWidth()
-    ) {
+    var rotation by remember { mutableStateOf(0f) }
+
+
+    val rotationAnimation = rememberInfiniteTransition()
+    val rotationValue by rotationAnimation.animateFloat(
+        initialValue = 0f,
+        targetValue = -360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 5000, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        )
+    )
+
+    rotation = rotationValue
+
+    Column(modifier = Modifier.fillMaxWidth()) {
         Box(
             modifier = Modifier.fillMaxWidth()
         ) {
             Icon(
                 modifier = Modifier
                     .size(28.dp)
-                    .align(Alignment.TopEnd).
-                    clickable { onTapFavorite(!isFavorite) },
+                    .align(Alignment.TopEnd)
+                    .clickable {
+                        onTapFavorite(!isFavorite)
+                    },
                 painter = painterResource(
                     id = if (isFavorite) {
                         R.drawable.ic_favorite_24
@@ -183,6 +246,9 @@ private fun Cover(
                     modifier = Modifier
                         .fillMaxWidth(0.6f)
                         .aspectRatio(1.0f)
+                        .graphicsLayer(
+                            rotationZ = rotation
+                        )
                         .align(Alignment.Center)
                         .clip(CircleShape),
                     contentScale = ContentScale.FillBounds
